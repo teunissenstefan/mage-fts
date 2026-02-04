31bb90c2b422c8b63a1a67c2421a45a6fdab6ae8
Stefan Teunissen <stefanteunissen1@gmail.com>
2026-02-04 20:04:54 +0100

    Check if app/etc/env.php exists

--- main.go
+++ main.go
@@ -18,7 +18,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Check if we're in a Magento root
+	envFile := "app/etc/env.php"
+	if _, err := os.Stat(envFile); os.IsNotExist(err) {
+		fmt.Fprintln(os.Stderr, "Error: app/etc/env.php not found")
+		os.Exit(1)
+	}
+
 	searchTerm := os.Args[1]
 	fmt.Printf("Searching for: %s\n", searchTerm)
 	fmt.Println("TODO")
 }
+
+
